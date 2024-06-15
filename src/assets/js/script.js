0a94e28ffc265395feb29f409e0e92d3843a8a5f
ilaria <ilariaferrauti@gmail.com>
2024-06-15 18:49:06 +0200

    fix swiper

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,23 +1,15 @@
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.min.css';
 
-const swiper = new Swiper(".swiper", {
+let swiper = new Swiper(".mySwiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+    dynamicBullets: true,
+    clickable: true,
   },
 });
+
+
